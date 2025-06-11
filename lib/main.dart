46d8773e9b15ac0677b4927ea9b0ec8982a93794
KennOtieno <otiken001@gmail.com>
2025-06-11 07:00:22 -0700

    Main.dart is error free

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: visualDensity.adaptivePlatformDensity,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
 
       home: const HomePage(),
